b9e78cde9e2e4e619db373b01ef7433dc69ccd42
falstack <icesilt@outlook.com>
2019-08-05 15:33:00 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,13 @@
-const isDev = process.env.NODE_ENV === 'development'
-
 module.exports = {
   mode: 'spa',
 
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#409eff' },
-
+  router: {
+    base: '/vue-hybird-best-practices/'
+  },
   /*
   ** Global CSS
   */
   css: [
     'normalize.css'
   ],
-
-  build: {
-    publicPath: isDev ? '/_nuxt/' : '/vue-hybird-best-practices/'
-  }
 }
